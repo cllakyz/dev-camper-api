4c05fd576203204a97df43f875083706d3cb4c8e
Celal <celal7174@gmail.com>
2020-07-14 23:05:09 +0300

    console colors

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express   = require('express');
-const connectDB = require('./config/db');
 const dotenv    = require('dotenv');
 const morgan    = require('morgan');
+const colors    = require('colors');
+const connectDB = require('./config/db');
 
 // Load env files
 dotenv.config({ path: './config/config.env' });
@@ -25,11 +26,11 @@ app.use('/api/v1/bootcamps', bootcampRouter);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () => { console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`) });
+const server = app.listen(PORT, () => { console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold) });
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red);
     // Close server & exit process
     server.close(() => process.exit(1));
 });

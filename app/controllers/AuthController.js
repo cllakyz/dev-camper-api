52904e84a20bf3d8e86f79f5b807e5c4ae63a98d
Celal <celal7174@gmail.com>
2020-08-19 16:20:46 +0300

    send cookie response

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -2,6 +2,28 @@ const ErrorResponse = require('./../utils/errorResponse');
 const asyncHandler  = require('./../middlewares/async');
 const User          = require('./../models/User');
 
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    const options = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+        options.secure = true;
+    }
+
+    res.status(statusCode)
+        .cookie('token', token, options)
+        .json({
+            success: true,
+            token
+        });
+}
+
 /**
  * @desc    Register user
  * @method  POST /api/v1/auth/register
@@ -20,13 +42,7 @@ exports.register = asyncHandler(async (req, res, next) => {
         role
     });
 
-    // Create token
-    const token = await user.getSignedJwtToken();
-
-    res.status(201).json({
-        success: true,
-        token
-    });
+    sendTokenResponse(user, 201, res);
 });
 
 /**
@@ -58,11 +74,5 @@ exports.login = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse('Invalid credentials', 401));
     }
 
-    // Create token
-    const token = user.getSignedJwtToken();
-
-    res.status(200).json({
-        success: true,
-        token
-    });
+    sendTokenResponse(user, 200, res);
 });

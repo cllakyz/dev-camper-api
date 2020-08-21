6d3b807072e5e4975c7a6ccad529019bcf7af3f9
Celal <celal7174@gmail.com>
2020-08-21 19:18:00 +0300

    update user details and password

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -172,3 +172,50 @@ exports.resetPassword = asyncHandler(async (req, res, next) => {
 
     sendTokenResponse(user, 200, res);
 });
+
+/**
+ * @desc    Update user details
+ * @method  PUT /api/v1/auth/updatedetails
+ * @access  Private
+ * @param   req
+ * @param   res
+ * @param   next
+ */
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+    const fieldsToUpdate = {
+        name: req.body.name,
+        email: req.body.email
+    };
+
+    const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
+
+/**
+ * @desc    Update password
+ * @method  PUT /api/v1/auth/updatepassword
+ * @access  Private
+ * @param   req
+ * @param   res
+ * @param   next
+ */
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select('+password');
+
+    // Check current password
+    if (!(await user.matchPassword(req.body.currentPassword))) {
+        return next(new ErrorResponse('Current password is incorrect', 401));
+    }
+
+    user.password = req.body.newPassword;
+    await user.save();
+
+    sendTokenResponse(user, 200, res);
+});
